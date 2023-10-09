4bd0756ad0f478d2e991197d7ee6bf64c9becc55
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-10-09 21:28:29 +0300

    Add files via upload

---
+++ LinqMethods/Program.cs
@@ -0,0 +1,69 @@
+﻿//Concat
+using System;
+
+var list1 = new List<int> { 1, 2, 3 };
+
+var list2 = new List<int> { 4, 5, 6 };
+
+var concatenatedList = list1.Concat(list2);
+
+foreach (var item in concatenatedList)
+{
+    Console.WriteLine(item);
+}
+
+
+//Distinct
+
+var numbersDistinct = new List<int> { 1, 2, 2, 3, 3, 4, 5 };
+
+var distinctNumbers = numbersDistinct.Distinct();
+
+foreach (var number in distinctNumbers)
+{
+    Console.WriteLine(number);
+}
+
+// Skip & Take
+var numbersSkipTake = Enumerable.Range(1, 10);
+
+var skippedAndTaken = numbersSkipTake.Skip(3).Take(4);
+
+foreach (var number in skippedAndTaken)
+{
+    Console.WriteLine(number);
+}
+
+// Sum
+var numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+var sum = numbers.Sum();
+
+Console.WriteLine(sum);
+
+
+// Repeat
+var repeatedNumbers = Enumerable.Repeat(5, 3);
+
+foreach (var number in repeatedNumbers)
+{
+    Console.WriteLine(number);
+}
+
+// Reverse 
+List<int> numbersReverse = new List<int> { 1, 2, 3, 4, 5 };
+numbersReverse.Reverse(); // Orijinal koleksiyonun sırasını tersine çevirir
+
+foreach (var number in numbersReverse)
+{
+    Console.WriteLine(number);
+}
+
+
+//min
+var numbersMin = new List<int> { 5, 2, 8, 1, 3 };
+
+var minNumber = numbersMin.Min();
+
+Console.WriteLine(minNumber);
+
